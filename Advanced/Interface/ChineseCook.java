61af22bf7186b10a457bd426edf93a65bee7811e
skywalker221-Minsoo <77948533+skywalker221-Minsoo@users.noreply.github.com>
2021-05-05 17:30:36 +0900

    Add files via upload

---
+++ Advanced/Interface/ChineseCook.java
@@ -0,0 +1,24 @@
+package Interface;
+
+public class ChineseCook implements ICook {// 인터페이스를 사용해서 좀 더 유연하게 상수, 추상메서드를 사용.
+
+	@Override // 각자의 여건에 맞추어서 재정의하여 사용.
+	public void makeRice() {
+
+	}
+
+	@Override
+	public void makeSoup() {
+
+	}
+
+	@Override
+	public void makeSalad() {
+
+	}
+
+	@Override
+	public void makeSource() {
+
+	}
+}
